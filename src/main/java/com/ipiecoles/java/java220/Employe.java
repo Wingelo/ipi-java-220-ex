de8f963fa91d2598d776d32b1fd846d4375f9e9a
Wingel <cedric.gamrat@gmail.com>
2018-11-27 15:17:13 +0100

    add new modif

--- src/main/java/com/ipiecoles/java/java220/Employe.java
+++ src/main/java/com/ipiecoles/java/java220/Employe.java
@@ -1,7 +1,127 @@
 package com.ipiecoles.java.java220;
 
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+import java.util.Objects;
+
 /**
  * Created by pjvilloud on 21/09/17.
  */
-public class Employe {
+public class Employe{
+
+    private String nom;
+    private String prenom;
+    private String matricule;
+    private LocalDate dateEmbauche;
+    private Double salaire;
+
+    public Employe(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.matricule = matricule;
+        this.dateEmbauche = dateEmbauche;
+        this.salaire = salaire;
+    }
+
+    public Employe(){
+
+    }
+
+
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employe employe = (Employe) o;
+        return Objects.equals(nom, employe.nom) &&
+                Objects.equals(prenom, employe.prenom) &&
+                Objects.equals(matricule, employe.matricule) &&
+                Objects.equals(dateEmbauche, employe.dateEmbauche) &&
+                Objects.equals(salaire, employe.salaire);
+    }
+    public int hashCode() {
+        return Objects.hash(nom, prenom, matricule, dateEmbauche, salaire);
+    }
+
+
+    public double augmenterSalaire()
+    @Override
+    public String toString() {
+        return "Employe{" +
+                "nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", matricule='" + matricule + '\'' +
+                ", dateEmbauche=" + dateEmbauche +
+                ", salaire=" + salaire +
+                '}';
+    }
+
+    public final Integer getNombreAnneeAnciennete(){
+        return LocalDate.now().getYear() - this.dateEmbauche.getYear();
+    }
+
+    public final Integer getNbConges(){
+        return Entreprise.NB_CONGES_BASE;
+    }
+
+
+
+
+
+
+
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public LocalDate getDateEmbauche() {
+        return dateEmbauche;
+    }
+
+    public Double getSalaire() {
+        return salaire;
+    }
+
+
+
+    // Setter
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public void setDateEmbauche(LocalDate dateEmbauche) throws Exception {
+        if(dateEmbauche.isAfter(LocalDate.now()))
+        {
+            throw new Exception("La date d'embauche ne peut être postérieure à la date courante");
+        }else{
+            this.dateEmbauche = dateEmbauche;
+        }
+
+    }
+
+    public void setSalaire(Double salaire) {
+        this.salaire = salaire;
+    }
+
+
 }
